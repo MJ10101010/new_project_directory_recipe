457ad595ed62021d23733d097ec6cb2c35e42e61
MJ10101010 <jargalan.michelle@gmail.com>
2026-01-16 18:22:23 -0500

    added recipe

---
+++ recipe.py
@@ -0,0 +1,13 @@
+# 1. Create an empty Github repository
+# 2. Open VScode terminal and create a new project folder
+# 3. Navigate/cd into the project folder
+# 4. Initialize git and create the main branch
+# 5. Create and activate a virtual environment
+# 6. Install necessary python packages
+# 7. Create requirements.txt from the installed packages
+# 8. Create a .gitignore file and add lines to ignore unnecessary files like:
+#       venv/
+#       __pycache__/
+#       *.pyc
+# 9. Add other project files, like recipe.py 
+# 10. Stage/add, commit, and push to github
